b05fea9fb2b103267fd73c3bdc6e2a53e6318385
Chris York <christopherayork1992@gmail.com>
2019-09-10 17:03:12 -0400

    Added id read endpoint

--- index.js
+++ index.js
@@ -34,6 +34,20 @@ app.route('/')
       });
   });
 
+app.get('/:id', (req, res) => {
+  let id = req.params.id;
+  db('cars').where({ id })
+    .then(r => {
+      console.log(r);
+      if(r) res.status(200).json(r);
+      else res.status(404).json({ error: 'Could not find the requested resource' });
+    })
+    .catch(e => {
+      console.error(e.response);
+      res.status(500).json({ errorMessage: 'The request could not be completed' });
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
